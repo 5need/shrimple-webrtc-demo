f88108768c3e6c64c45144527e68d0776374359a
mccullen <mccullen@mccullen.xyz>
2025-04-15 19:03:16 -0500

    stuff... again

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
-console.log("Hello via Bun!");
+import express from "express";
+import expressWs from "express-ws";
+import { v4 as uuidv4 } from "uuid";
 
+const app = express();
+expressWs(app);
+
+const websocketClients = new Map();
+
+app.ws("/ws", (ws, req) => {
+  // ws client connected
+  const id = uuidv4();
+  websocketClients.set(id, ws);
+  console.log("WebSocket connected");
+
+  ws.on("message", (msg) => {
+    console.log("message:", msg);
+    ws.send(`echo: ${msg}`);
+  });
+
+  ws.on("close", () => {
+    console.log("WebSocket disconnected");
+  });
+
+  ws.send(id + "connected to /ws");
+});
+
+app.get("/", (_, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on http://localhost:3000");
+});
